6343674b4f89f975e5a5760f39aa246f64a80b15
Rabin BK <aarsis717@gmail.com>
2019-03-22 21:13:10 +0545

    Convert Celsius to Fahrenheit

---
+++ Q2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 11:13:38 2019
+
+@author: kingoking
+"""
+#The eval function accepts the value of any data type
+celsius = eval(input("Enter value in celsius: ")) #taking input from the console
+fahrenheit = (9/5)*celsius+32
+print("Equivalent celsius value in Fahrenheit is: ", fahrenheit)
