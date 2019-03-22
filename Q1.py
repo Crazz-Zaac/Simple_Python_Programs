66c8b182f83921c5a23556476e70f3afde939229
Rabin BK <aarsis717@gmail.com>
2019-03-22 21:12:02 +0545

    Area and perimeter of a rectangle

---
+++ Q1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 08:31:53 2019
+
+@author: kingoking
+"""
+
+l = 4.5 #Value of length and breadth is hard-code
+b = 7.9
+A = l*b
+P = 2*(l+b)
+print("Area is: ",A)
+print("Perimeter is: ",P)
